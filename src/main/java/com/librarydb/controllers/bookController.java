8dd104ed7798a0b6d6a71f03be9aac62a6f1278c
J. Nathan Allen <nateonmission@gmail.com>
2022-01-03 15:48:48 -0500

    got spring to run with hello world

---
+++ src/main/java/com/librarydb/controllers/bookController.java
@@ -0,0 +1,21 @@
+package com.librarydb.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/api")
+public class bookController {
+    private static final Logger LOGGER = Logger.getLogger(bookController.class.getName());
+
+    @GetMapping("/")
+    public String Hello(){
+        LOGGER.info("calling getCategory method from controller");
+        return "Hello World";
+    }
+
+}
