a9deba609133e57c53d0c6fbdfde1d19d3b84c53
Abby Henes <abbyhenes@gmail.com>
2022-01-04 09:31:42 -0600

    created publisher and genre repositories

--- src/main/java/com/librarydb/repositories/GenreRepository.java
+++ src/main/java/com/librarydb/repositories/GenreRepository.java
@@ -4,4 +4,5 @@ import com.librarydb.models.Genres;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GenreRepository extends JpaRepository<Genres, Long> {
+    Genres findByName(String genresName);
 }
--- src/main/java/com/librarydb/repositories/PublisherRepository.java
+++ src/main/java/com/librarydb/repositories/PublisherRepository.java
@@ -4,4 +4,5 @@ import com.librarydb.models.Publishers;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PublisherRepository extends JpaRepository<Publishers, Long> {
+    Publishers findByName(String publishersName);
 }
