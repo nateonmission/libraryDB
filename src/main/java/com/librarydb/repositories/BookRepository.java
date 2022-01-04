63d4b01da6f8bd67c8a4835f7baa6123e515d357
Abby Henes <abbyhenes@gmail.com>
2022-01-04 09:29:02 -0600

    created book repository

--- src/main/java/com/librarydb/repositories/BookRepository.java
+++ src/main/java/com/librarydb/repositories/BookRepository.java
@@ -3,5 +3,10 @@ package com.librarydb.repositories;
 import com.librarydb.models.Books;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Books, Long> {
+    Books findById(String booksTitle);
+    Books findByTitle(String booksTitle);
+    List<Books> findByAuthorId(Long authorsId);
 }
