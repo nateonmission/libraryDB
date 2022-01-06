a84c9d13448a02b8894f16fc329660c774bc0504
J. Nathan Allen <nateonmission@gmail.com>
2022-01-06 18:00:23 -0500

    started DB tests

--- src/test/java/com/librarydb/LibraryDbApplicationTests.java
+++ src/test/java/com/librarydb/LibraryDbApplicationTests.java
@@ -1,24 +1,38 @@
 package com.librarydb;
 
 import com.librarydb.controllers.bookController;
+import com.librarydb.models.Books;
+import com.librarydb.repositories.BookRepository;
 import com.librarydb.services.BookServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 class LibraryDbApplicationTests {
 
-    @LocalServerPort
-    private int port;
+//    @LocalServerPort
+//    private int port;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+//    @Autowired
+//    private TestRestTemplate restTemplate;
 
     @Autowired
     private bookController controller;
@@ -26,6 +40,11 @@ class LibraryDbApplicationTests {
     @MockBean
     private BookServices services;
 
+    private BookRepository bookRepository;
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     // BASIC CONNECTION TESTS
     @Test
@@ -34,16 +53,18 @@ class LibraryDbApplicationTests {
     }
 
     @Test
-    public void greetingShouldReturnMessageFromService() throws Exception {
-        when(services.isAlive()).thenReturn("<h1>I'm Alive</h1>");
+    public void testDBSave() {
+        Books book = new Books();
+        String title = "Testing the Test4";
+        book.setTitle(title);
+        book.setIsbn10("1234567890");
+        book.setIsbn13("1234567890123");
+        bookRepository.save(book);
+        Books testBook = bookRepository.findByTitle(title);
+        assertNotNull(bookRepository.findById(testBook.getId()).get());
+        bookRepository.delete(book);
     }
 
-    // GET all from each model
-
-    @Test
-    public void getAllAuthors() throws Exception {
-        when(services.getAuthors()).thenReturn();
-    }
 
 
 
